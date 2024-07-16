89bc705d0953472023bcbc2c0290d8b4ccd321a7
Yukiko Matsuda <151450228+YukikoMatsuda@users.noreply.github.com>
2024-07-17 03:05:53 +0900

    Update models.py
    やっぱこれから起点に考える

--- src/models.py
+++ src/models.py
@@ -5,26 +5,31 @@ from einops.layers.torch import Rearrange
 
 
 class BasicConvClassifier(nn.Module):
-    def __init__(self, num_classes, seq_len, num_channels, dropout_rate=0.5):
+    def __init__(self, num_classes, seq_len, num_channels):
         super(BasicConvClassifier, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=num_channels, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(64 * (seq_len // 4) * (seq_len // 4), 128)  # Poolingでサイズが1/4になると仮定
-        self.fc2 = nn.Linear(128, num_classes)
-        self.dropout = nn.Dropout(p=dropout_rate)
+        self.conv1 = nn.Conv1d(in_channels=num_channels, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm1d(64)
+        self.conv2 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.conv3 = nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm1d(256)
+        self.fc1 = nn.Linear(256 * (seq_len // 8), 256)
+        self.fc2 = nn.Linear(256, num_classes)
+        self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.max_pool1d(x, 2)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.max_pool1d(x, 2)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.max_pool1d(x, 2)
         x = x.view(x.size(0), -1)  # Flatten
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
         return x
-
-
+        
 class ConvBlock(nn.Module):
     def __init__(
         self,
